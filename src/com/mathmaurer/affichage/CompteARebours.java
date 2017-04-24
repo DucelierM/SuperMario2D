6ed9028a4e603fb29145313dd5831ee0b1fb9400
DucelierM <mickaelducelier@hotmail.fr>
2017-04-24 14:57:09 +0200

    Permet de couper le temps lorsque Mario meurt

--- src/com/mathmaurer/affichage/CompteARebours.java
+++ src/com/mathmaurer/affichage/CompteARebours.java
@@ -1,49 +1,48 @@
 package com.mathmaurer.affichage;
 
-import com.mathmaurer.personnages.Mario;
-
 public class CompteARebours implements Runnable{
 
 	// VARIABLES
-	private final int PAUSE = 1000;	// Est égale a une seconde 
-	private final int STOP = 1000;
+	private final int PAUSE = 1000;	// 1000MilliSecondes = 1Seconde, il nous permet de faire le décompte en secondes.
 	private int compteurTemps;
 	private String str;
-	public Mario marioDead = new Mario(28, 30);
-	
-	
+	public boolean tempsPause;
+		
 	// CONSTRUCTEUR
 	public CompteARebours(){
-		this.compteurTemps = 100; 
-		this.str = "Temps restant : 100";		
+		this.compteurTemps = 120; 
+		this.str = "Temps restant : 120";		
 		Thread compteARebours = new Thread(this);
 		compteARebours.start();
 	}
-
 	
 	// GETTERS	
+	
 	public int getCompteurTemps() {return compteurTemps;}
 	
     public String getStr() {return str;}
 
-	
+	public boolean isTempsPause() {return tempsPause;}
+    
     // SETTERS
     
-    
+	public void setStr(String str) {this.str = str;}
+
+	public void setCompteurTemps(int compteurTemps) {this.compteurTemps = compteurTemps;}
+	
+	public void setTempsPause(boolean essai) {this.tempsPause = essai;}
+
 	// METHODES	
+	
 	@Override
 	public void run() {				
 		while(true){	
-			while(this.compteurTemps > 0){
+			while(this.compteurTemps > 0 && tempsPause == false){
 			    try{Thread.sleep(PAUSE);}
 				catch (InterruptedException e){}
 				this.compteurTemps--;
 				this.str = "Temps restant : " + this.compteurTemps;
 			}
 		}		
-	}
-	
-	public void stopTime(){
-		this.str = "Temps restant : " + this.compteurTemps;
-	}
+	}	
 }
